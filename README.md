894ae11051346e0c6f0a6dff0f5113c913aa0633
yaelbh <yaelbh@il.ibm.com>
2018-06-10 11:48:50 +0300

    Launched a new repository qiskit-addon-projectq

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# qiskit-addon-projectq
-Pluggable ProjectQ simulator backend for QISKit Core
+# QISKit simulators based on ProjectQ
+
+This module contains [QISKit](https://www.qiskit.org/) simulators whose backends are written in ProjectQ. These simulators simulate a quantum circuit on a classical computer.
+
+## Installation
+
+Install this module via the PIP tool:
+
+```
+pip install qiskit_addon_projectq
+```
+
+PIP will handle all dependencies automatically.
+
+## Usage
+
+We recommend to follow the [usage example](examples/projectq_backends.py). More general information and education on running quantum simulation can be found in the [QISKit instructions page](https://github.com/QISKit/qiskit-core) and the QISKit tutorials.
+
+## License
+
+This project uses the [Apache License Version 2.0 software license](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 IBM RESEARCH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qiskit_addon_projectq",
+    version="0.1.0",
+    author="QISKit Development Team",
+    author_email="qiskit@us.ibm.com",
+    description="QISKit simulators whose backends are written in PorjectQ",
+    long_description = "This module contains [QISKit](https://www.qiskit.org/) simulators whose backends are written in ProjectQ. These simulators simulate a Quantum circuit on a classical computer.",
+    url="https://github.com/QISKit/qiskit-addon-projectq",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=['qiskit>=0.5', 'projectq>=0.3.6'],
+    keywords="qiskit quantum projectq simulator",
+    packages=find_packages(exclude=['test*']),
+    include_package_data=True,
+    python_requires=">=3.5"
+)
