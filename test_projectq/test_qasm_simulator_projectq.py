d1fe3179c471f6d1642818287c3563e6aeb88c9f
yaelbh <yaelbh@il.ibm.com>
2018-06-19 17:13:40 +0300

    Fixed a linter error (#3)

--- test_projectq/test_qasm_simulator_projectq.py
+++ test_projectq/test_qasm_simulator_projectq.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=invalid-name,missing-docstring,broad-except
+# pylint: disable=invalid-name,missing-docstring,broad-except,no-member
 
 # Copyright 2017 IBM RESEARCH. All Rights Reserved.
 #
